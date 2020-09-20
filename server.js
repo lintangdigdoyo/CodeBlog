e6af98523c4d6ab06297e2bb585952efe016d3e6
lintangdigdoyo <digdoyo.lintang@gmail.com>
2020-09-20 11:15:21 +0700

    modify server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https');
 const express = require('express');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
@@ -7,6 +8,7 @@ const path = require('path');
 
 const app = express();
 
+let server;
 if (process.env.NODE_ENV !== 'production') {
   const cors = require('cors');
   require('dotenv').config({ path: './config/.env' });
@@ -16,9 +18,11 @@ if (process.env.NODE_ENV !== 'production') {
       credentials: true,
     })
   );
+  server = http.createServer(app);
+} else if (process.env.NODE_ENV === 'production') {
+  server = https.createServer(app);
 }
 
-const server = http.createServer(app);
 const io = socketio(server);
 
 //Connect Databases
