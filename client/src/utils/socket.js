8da42d750bd420ef7f15d5117a0eb6d7cd2e60ec
lintangdigdoyo <digdoyo.lintang@gmail.com>
2020-09-20 13:04:11 +0700

    socketio utils production

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,9 @@
 import io from 'socket.io-client';
 
-const ENDPOINT = process.env.NODE_ENV === 'development' ? 'localhost:5000' : '';
+const ENDPOINT =
+  process.env.NODE_ENV === 'development'
+    ? 'localhost:5000'
+    : 'https://appcodeblog.herokuapp.com/';
 const socket = io(ENDPOINT);
 
 export default socket;
